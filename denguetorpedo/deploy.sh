b08c7413ca195a973aaede26fd0117699322e981
AppCivist <appcivist@js-156-246.jetstream-cloud.org>
2018-03-28 23:19:23 -0400

    Added a line to delete old pids files

--- denguetorpedo/deploy.sh
+++ denguetorpedo/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/dengue/denguetorpedo
 bundle install
-rails server
+kill -9 `cat /home/dengue/denguetorpedo/tmp/pids/server.pid`
+rm /home/dengue/denguetorpedo/tmp/pids/server.pid
+rails server -p 3001 
