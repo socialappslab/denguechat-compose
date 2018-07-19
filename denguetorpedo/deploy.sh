2fd652a6f877fe20f98eb8e2e9de13106167bc98
AppCivist <appcivist@js-156-149.jetstream-cloud.org>
2018-07-19 13:54:05 -0400

    Restart always and rm pid file
    Signed-off-by: AppCivist <appcivist@js-156-149.jetstream-cloud.org>

--- denguetorpedo/deploy.sh
+++ denguetorpedo/deploy.sh
@@ -2,5 +2,5 @@
 cd /home/dengue/denguetorpedo
 bundle install
 kill -9 `cat /home/dengue/denguetorpedo/tmp/pids/server.pid`
-rm /home/dengue/denguetorpedo/tmp/pids/server.pid
+rm -f /home/dengue/denguetorpedo/tmp/pids/server.pid
 bundle exec foreman start
