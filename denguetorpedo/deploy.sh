0ce6d6821e3a39421752d2ace8c0ef6e17b3cc6c
Cristhian Parra <cdparra@gmail.com>
2019-07-16 19:03:28 -0400

    Use just one branch and different docker-compose files for dev and production

--- denguetorpedo/deploy.sh
+++ denguetorpedo/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/dengue/denguetorpedo
 bundle install
-#kill -9 `cat /home/dengue/denguetorpedo/tmp/pids/server.pid`
-#rm -f /home/dengue/denguetorpedo/tmp/pids/server.pid
+kill -9 `cat /home/dengue/denguetorpedo/tmp/pids/server.pid`
+rm -f /home/dengue/denguetorpedo/tmp/pids/server.pid
 bundle exec foreman start
