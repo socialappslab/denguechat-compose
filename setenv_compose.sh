cc11a5ec8c3b60ad61820f027be289b6bbda382e
Cristhian Parra <cdparra@gmail.com>
2019-07-30 13:16:26 -0400

    gitflow-feature-stash: postgres_container

--- setenv_compose.sh
+++ setenv_compose.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Replace the following command with your the command that works well in your host machine
+# replace this with your local IP getting command
 IP=`ifconfig | grep -A 1 'en0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 2` 
 export HOST_IP=$IP
 echo "HOST_IP environment variable set to => " $HOST_IP
-
